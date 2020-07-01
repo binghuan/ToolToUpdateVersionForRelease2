0aa6fd76a2b22a7c3a93d4deb12b5f2c4b22ab5e
BH_Lin <binghuanlin@gmail.com>
2020-07-02 01:10:17 +0800

    update script

--- updateversion.sh
+++ updateversion.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# if [ "$#" != 1 ]; then
+#     echo "–"
+#     echo "⚠ You must give me arguments for setup.\n"
+#     echo "  ☞ Arg[1]: build Stage , ex: SIT/PRO/DEV"
+#     echo "–"
+#     exit 1
+# fi
+
 pacakgeFile="app/package.json"
+configFile="resources/mac/config.json"
 versionString=$(cat ${pacakgeFile} | grep -i -E "version")
 echo "Check current version in file ${pacakgeFile}"
 echo "${versionString}"
@@ -26,11 +35,53 @@ sed "s/\"version\": \"${originalVersion}\",/\"version\": \"${newVersion}\",/g" $
 mv -f tmp.json ${pacakgeFile}
 echo "OK> Version has been updated."
 
-echo "Ready to release new version for build stage SIT"
-today=$(date +"%Y%m%d")
-tagForSIT="SIT_${today}_"
-echo "${tagForSIT}"
-totalReleaseNumber=$(git tag -l "${tagForSIT}*" | wc -l)
-targetReleaseNumber=$(($totalReleaseNumber + 1))
-tagForSIT="SIT_${today}_${targetReleaseNumber}"
-echo "New Tag = ${tagForSIT}"
+function commitWithTag() {
+    echo "[START]-------------------------------------------------------------->"
+    date 
+    echo ""
+    buildStage=$1
+    echo "commitWithTag for build stage ${buildStage}"
+    shortName=""
+    case "$buildStage" in
+    "SIT")
+        echo "build stage is SIT"
+        shortName="s"
+        ;;
+    "PRO")
+        echo "build stage is PRODUCTION"
+        shortName="p"
+        ;;
+    "DEV")
+        echo "build stage is DEV"
+        shortName="d"
+        ;;
+    *)
+        echo "build stage is UNKNOWN"
+        shortName=""
+        exit 2
+        ;;
+    esac
+
+    echo "{\"PLATFORM\": \"${shortName}\"}" >${configFile}
+    git add ${configFile}
+    git add ${pacakgeFile}
+    git status
+    git commit -m "Update version to release ${newVersion}"
+    git push
+
+    echo ">> tagForBuildStage: "
+    echo "Ready to release new version for build stage ${buildStage}"
+    YYYYMMDD=$(date +"%Y%m%d")
+    tagForBuildStage="${buildStage}_${YYYYMMDD}_"
+    echo "${tagForBuildStage}"
+    totalReleaseNumber=$(git tag -l "${tagForBuildStage}*" | wc -l)
+    targetReleaseNumber=$(($totalReleaseNumber + 1))
+    tagForBuildStage="${buildStage}_${YYYYMMDD}_${targetReleaseNumber}"
+    echo "New Tag = ${tagForBuildStage}"
+    git tag -a ${tagForBuildStage} -m "Add tag to release ${newVersion}"
+    git push --tags
+    echo "[END]----------------------------------------------------------------<\n"
+}
+
+commitWithTag SIT
+commitWithTag PRO
